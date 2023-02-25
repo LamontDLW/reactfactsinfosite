c026159b70fc3ad9aa72e5b1612625d9f399fca9
LamontDLW <120749833+LamontDLW@users.noreply.github.com>
2023-02-24 21:21:35 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import './styles/index.css'
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
      <div className="container">
         <Navbar />
         <Main />
+        <Footer />
      </div>
     )
 }
